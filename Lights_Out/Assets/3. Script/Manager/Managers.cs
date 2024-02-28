edcefbb484e0a51393ac667ae994b8a038fd297b
magicJSC <jeonghaejun004@gmail.com>
2024-02-28 13:53:57 +0900

    GameManager

--- Lights_Out/Assets/3. Script/Manager/Managers.cs
+++ Lights_Out/Assets/3. Script/Manager/Managers.cs
@@ -7,11 +7,11 @@ public class Managers : MonoBehaviour
     static Managers _instance;
     public static Managers Instance { get { return _instance; } }
 
-    //GameManager _game = new GameManager();
-    //public static GameManager Game { get { return _instance._game; } }
+    GameManager _game = new GameManager();
+    public static GameManager Game { get { return _instance._game; } }
 
     InputManager _input = new InputManager();
-    public static InputManager Input { get { return Instance._input; } }
+    public static InputManager Input { get { return _instance._input; } }
 
     private void Awake()
     {
