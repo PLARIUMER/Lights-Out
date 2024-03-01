24f167dd01f3ff9cc12ac9178fea02bf186e2439
JunghyunByun <bjh1206408@gmail.com>
2024-03-01 20:39:00 +0900

    Light

--- Lights_Out/Assets/3. Script/Manager/CursorManager.cs
+++ Lights_Out/Assets/3. Script/Manager/CursorManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class CursorManager : MonoBehaviour
 {
     [SerializeField] private Texture2D cursorTexture;
+    [SerializeField] private Transform targetObject;
     private Vector2 cursorHotspot;
 
-    void Start()
+    private void Update()
     {
         cursorHotspot = new Vector2(cursorTexture.width / 2, cursorTexture.height / 2);
 
         Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
+
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cursorPos.z = 0f; 
+
+        targetObject.position = cursorPos;
     }
 }
